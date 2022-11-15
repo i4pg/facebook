b132916634373a882644c9fa35f6b17e13b9bf8d
xi <08h4b21i@anonaddy.me>
2022-11-15 18:31:43 +0400

    Add Timeago featur for posts

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,13 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+// import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
+
+import { Application } from '@hotwired/stimulus'
+import Timeago from 'stimulus-timeago'
+
+const application = Application.start()
+application.register('timeago', Timeago)
